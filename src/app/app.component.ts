6680b5e6da9176850728eb26f3a4a8dd770f095f
Harald Nezbeda <hn@anexia.at>
2018-11-28 12:14:19 +0100

    Use ng-select scss instead of css for default theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,17 @@ export class AppComponent implements OnInit {
       'test_textarea': new FormControl('', Validators.required),
       'test_checkbox': new FormControl(false, Validators.requiredTrue),
       'test_radio': new FormControl(false),
+      'test_select': new FormControl(null, Validators.requiredTrue),
     });
   }
+
+  selectOptions = [
+    {pk: 1, label: 'Option 1'},
+    {pk: 2, label: 'Option 2'},
+    {pk: 3, label: 'Option 3'},
+    {pk: 4, label: 'Option 4'},
+    {pk: 5, label: 'Option 5'}
+  ]
 
   markAsTouched() {
     for (let inner in this.form.controls) {
