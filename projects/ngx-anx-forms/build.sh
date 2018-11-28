27b8be054442358f7d339f4c5bc4271ffd609683
Harald Nezbeda <hn@anexia.at>
2018-11-28 12:28:33 +0100

    Update build script, add publish script

--- projects/ngx-anx-forms/build.sh
+++ projects/ngx-anx-forms/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm -r ../../dist/ngx-anx-forms/
 ng build ngx-anx-forms
 cp -r src/lib/styles/ ../../dist/ngx-anx-forms/lib/styles/
---
+++ publish.sh
@@ -0,0 +1,4 @@
+cd projects/ngx-anx-forms
+bash build.sh
+cd ../../dist/ngx-anx-forms
+npm publish
